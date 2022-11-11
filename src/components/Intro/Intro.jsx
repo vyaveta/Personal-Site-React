b223806b2c85fbcac0fd9d70dacddcd9af9096bd
Adwaith <108970016+vyaveta@users.noreply.github.com>
2022-11-11 16:12:42 +0530

    first commit

---
+++ src/components/Intro/Intro.jsx
@@ -0,0 +1,94 @@
+import React, { useContext } from "react";
+import "./Intro.css";
+import Vector1 from "../../img/Vector1.png";
+import Vector2 from "../../img/Vector2.png";
+import boy from "../../img/boy.png";
+import glassesimoji from "../../img/glassesimoji.png";
+import thumbup from "../../img/thumbup.png";
+import crown from "../../img/crown.png";
+import FloatinDiv from "../FloatingDiv/FloatingDiv";
+import Github from "../../img/github.png";
+import LinkedIn from "../../img/linkedin.png";
+import Instagram from "../../img/instagram.png";
+import { themeContext } from "../../Context";
+import { motion } from "framer-motion";
+import { Link } from "react-scroll";
+const Intro = ({pStart , pEnd}) => {
+  // Transition
+  const transition = { duration: 2, type: "spring" };
+
+  // context
+  const theme = useContext(themeContext);
+  const darkMode = theme.state.darkMode;
+
+  return (
+    <div className="Intro" id="Intro">
+      {/* left name side */}
+      <div className="i-left">
+        <div className="i-name">
+          {/* yahan change hy darkmode ka */}
+          <span style={{ color: darkMode ? "white" : "" }}>Hy! I Am</span>
+          <span>Adwaith!</span>
+          <span className='p-tag'>{pStart} <span className='intro-txt' >Self taught MERN stack developer who spends most of his time staring at the code and now you are staring at the Adwaith's portfolio made using React. </span> <span className='p-tag'>{pEnd}</span></span>
+        </div>
+        <Link to="contact" smooth={true} spy={true}>
+          <button className="button i-button">Hire me</button>
+        </Link>
+        {/* social icons */}
+        <div className="i-icons">
+          <img src={Github} alt="" />
+          <img src={LinkedIn} alt="" />
+          <img src={Instagram} alt="" />
+        </div>
+      </div>
+      {/* right image side */}
+      <div className="i-right">
+        <img src={Vector1} alt="" />
+        <img src={Vector2} alt="" />
+        <img src='' alt="" />
+        {/* animation */}
+        <motion.img
+          initial={{ left: "-36%" }}
+          whileInView={{ left: "-24%" }}
+          transition={transition}
+          src={glassesimoji}
+          alt=""
+        />
+
+        <motion.div
+          initial={{ top: "-4%", left: "74%" }}
+          whileInView={{ left: "68%" }}
+          transition={transition}
+          className="floating-div"
+        >
+          <FloatinDiv img={crown} text1="MERN" text2="Stack" />
+        </motion.div>
+
+        {/* animation */}
+        <motion.div
+          initial={{ left: "9rem", top: "18rem" }}
+          whileInView={{ left: "0rem" }}
+          transition={transition}
+          className="floating-div"
+        >
+          {/* floatinDiv mein change hy dark mode ka */}
+          <FloatinDiv img={thumbup} text1="Self" text2="Taught" />
+        </motion.div>
+
+        <div className="blur" style={{ background: "rgb(238 210 255)" }}></div>
+        <div
+          className="blur"
+          style={{
+            background: "#C1F5FF",
+            top: "17rem",
+            width: "21rem",
+            height: "11rem",
+            left: "-9rem",
+          }}
+        ></div>
+      </div>
+    </div>
+  );
+};
+
+export default Intro;
